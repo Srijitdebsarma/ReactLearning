a07962e47be8c1bacacb41a2998da3f0e5c6c525
srijitDebsarma <srijitdebsarma@gmail.com>
2024-05-21 11:57:52 +0530

    Multiple Form Elements controlled by a object of Variables

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,19 +1,58 @@
-import { useState } from "react"
+import { useState } from "react";
 
-export default function Form(){
-    let [fullName,setFullName]=useState("");
-    let handleNameChange=(event)=>{
-        console.log(event.target.value);
-        setFullName(event.target.value);
-    }
-    let onSubmit=(event)=>{
-        event.preventDefault();
-        console.log(fullName);
-    }
-    return(
-        <form onSubmit={onSubmit}>
-            <input placeholder="Enter your name" type="text" value={fullName} onChange={handleNameChange}></input>
-            <button type="submit">Submit</button>
-        </form>
-    )
+export default function Form() {
+//Making an Object of State Variables
+  let [formData, setFormData] = useState({
+    username:"",
+    gmail:""
+  });
+
+  let handleChange = (event) => {
+    let fieldName=event.target.name;    //fieldName Consist with StateVariable Object keys
+    let newValue=event.target.value;    //newValue consist of the new the newValue what we give
+    setFormData((currdata)=>{
+        return {...currdata, [fieldName]:newValue };    
+        //all the data Must same, just jei key tew change hych setar value ta k change koro
+    })
+  };
+  
+
+
+  let onSubmit = (event) => {
+    //just print them into console
+    event.preventDefault();
+    console.log("Username:"+formData.username + "    "+"Mail:"+ formData.gmail);
+    //clean the data while the form got submitted
+    setFormData({
+        username:"",
+        gmail:""
+      })
+  };
+
+
+
+  return (
+    <form onSubmit={onSubmit}>
+      <label htmlFor="username">Username</label>
+      <input
+        placeholder="Enter your name"
+        type="text"
+        value={formData.username}
+        onChange={handleChange}
+        id="username"
+        name="username"
+      ></input>
+      <br></br>
+      <label htmlFor="gmail">Gmail</label>
+      <input
+        placeholder="Enter your gmail"
+        type="email"
+        value={formData.gmail}
+        onChange={handleChange}
+        id="gmail"
+        name="gmail"
+      ></input>
+      <button type="submit" style={{backgroundColor:"red"}}>Submit</button>
+    </form>
+  );
 }
